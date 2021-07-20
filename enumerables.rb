fb8b4c6592f842da4ce0fde570c4cce209323edd
Danny Park <danincali@gmail.com>
2021-07-19 17:15:36 -0700

    Enumerables v1

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,35 @@
-p 'hello'
+class Array
+
+    def my_each(&prc)
+    
+        i=0
+        while i<self.length
+            self[i] = prc.call(self[i] )
+            i+=1
+        end
+
+        self
+    
+    end
+
+
+    def my_select(&prc)
+        arr = []
+        self.my_each do |ele|
+            arr << ele if prc.call(ele)
+        end
+        arr
+    end
+
+
+    def my_reject(&prc)
+        arr = []
+        self.my_each do |ele|
+            arr << ele if !prc.call(ele)
+        end
+        arr
+    end
+
+    
+
+end
